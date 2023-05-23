a43a35aadfedfa30a25b89ad3bcc55bb243f7381
Nuwan <nuwanchaturanga@gmail.com>
2023-05-23 19:06:40 +0530

    fix banner and poster image url

--- src/assets/js/helpers/eventHelper.js
+++ src/assets/js/helpers/eventHelper.js
@@ -3,7 +3,7 @@ import { env } from "../config";
 export const posterImageUrl = (poster) => {
   const posterImg = poster.data.attributes.formats.small
   if(process.env.NODE_ENV === 'production'){
-    return `${env('DIGITAL_OCEAN_SPACE_ENDPOINT')}/${posterImg.hash}.${posterImg.ext}`
+    return `${env('DIGITAL_OCEAN_SPACE_ENDPOINT')}/${posterImg.hash}${posterImg.ext}`
   }else{
     return `${env('STRAPI_BASE_URL')}${posterImg.url}`
   }
@@ -13,7 +13,7 @@ export const posterImageUrl = (poster) => {
 export const bannerImageUrl = (banner) => {
   const bannerImg = banner.data.attributes.formats.large
   if(process.env.NODE_ENV === 'production'){
-    return `${env('DIGITAL_OCEAN_SPACE_ENDPOINT')}/${bannerImg.hash}.${bannerImg.ext}`
+    return `${env('DIGITAL_OCEAN_SPACE_ENDPOINT')}/${bannerImg.hash}${bannerImg.ext}`
   }else{
     return `${env('STRAPI_BASE_URL')}${bannerImg.url}`
   }
